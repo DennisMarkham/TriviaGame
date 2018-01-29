7890522e3abd188a4b5bc2c08a314c3d2e86f563
unknown <workdennis@yahoo.com>
2018-01-28 21:29:41 -0600

    I am committing

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,35 @@
+
+var unanswered = 10;
+var correct = 0
+var wrong = 0
+
+
+var question1 = document.myForm.q1.value;
+	var question2 = document.myForm.q2.value;
+	var question3 = document.myForm.q3.value;
+	var correct = 0;
+
+console.log(question1);
+
+$(".correct").on("click", function () {
+correct++;
+unanswered--;
+console.log(correct);
+});
+
+$(".wrong").on("click", function () {
+wrong++;
+unanswered--;
+console.log(wrong);
+});
+
+
+
+setTimeout(function(){
+
+        $("body").text("Times up.  You got " + correct + " correct, " + wrong + " wrong, and left " + unanswered + " unanswered.");
+        console.log("test");
+      }, 60 * 1000);
+
+
+
